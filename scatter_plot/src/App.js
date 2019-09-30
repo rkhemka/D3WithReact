a12fc1812366633b9e41aa522b382f34544ed93b
rkhemka <rajatkhemka66@gmail,com>
2019-09-30 07:26:27 -0700

    initial commit

---
+++ scatter_plot/src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { json } from "d3"
+import ChartWrapper from './ChartWrapper';
+import Table from './Table';
+
+class App extends Component {
+  state = {
+    data: [],
+    activeName:null
+  }
+  componentWillMount() {
+    json("https://udemy-react-d3.firebaseio.com/children.json")
+      .then(data => this.setState({ data }))
+      .catch(error => console.log(error))
+  }
+  updateName = (activeName) => this.setState({ activeName })
+
+  updateData = (data) => this.setState({ data })
+  render() {
+    return (
+      <div>
+        <Navbar bg="light">
+          <Navbar.Brand>CreateD3App</Navbar.Brand>
+        </Navbar>
+        <Container>
+          {this.state.data.length>0 &&<Row>
+            <Col md={6} xs={12}><ChartWrapper data={this.state.data} updateName={this.state.updateName} /></Col>
+            <Col md={6} xs={12}><Table data={this.state.data} updateData={this.updateData} activeName={this.state.activeName} /></Col>
+          </Row>}
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default App;
